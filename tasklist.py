885fe2487a351c47a11c862f7d99a29dcb310bf8
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2024-01-09 02:17:52 -0300

    List tasks

--- tasklist.py
+++ tasklist.py
@@ -1,7 +1,15 @@
 tasks = []
 
+def list_task(tasks):
+  print("\nTask list: ")
+  for indice, task in enumerate(tasks, start=1):
+    status = "✓" if task["completed"] else " "
+    task_name = task["task"]
+    print(f"{indice}. [{status}] {task_name}")
+  return
+
 def add_task(tasks, task_name):
-  task = {"task": task_name, "Completed": False}
+  task = {"task": task_name, "completed": False}
   tasks.append(task)
   print(f"The task {task} was successfully added")
   return
@@ -9,7 +17,7 @@ def add_task(tasks, task_name):
 while True:
   print("\nTask Manager Menu: ")
   print("1. Add task")
-  print("2. See one task")
+  print("2. See all tasks")
   print("3. Update task")
   print("4. Mark task as done")
   print("5. Delete completed task")
@@ -20,6 +28,8 @@ while True:
   if choice == "1":
     task_name = input("Type the name of the task: ")
     add_task(tasks, task_name)
+  elif choice == "2":
+    list_task(tasks)
   elif choice == "6":
     break
 
